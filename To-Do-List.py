8f692e2ab83bb50d5d2cfd1a507459d963daa69d
Nida <nidaarshad309@gmail.com>
2026-08-02 09:12:06 +0500

    Add 'add-task' and 'view-task' Functions

--- To-Do-List.py
+++ To-Do-List.py
@@ -1,15 +1,39 @@
+#empty list
+todo_task=[]
+
+def add_task():             #Function to ADD TASkS
+    task=input("Enter a Task: ")
+    todo_task.append({"Task": task, "Status": "Pending"})
+    print("Task Added Successfully ")
+    print("\n")
+
+def view_task():            #Function to View TAsks
+    
+    print("\n=======Your To-Do-List=======")
+    if len(todo_task)==0:
+        print("No pending Task..")
+    else:
+        for index,task in enumerate(todo_task,1):
+            print(f"{index}: {task['Task']} - {task['Status']}")
+    print("\n")
+
+
+
+
 
 def display_menu():
+
     print("====== TO-DO-LIST ======")
 
 
     print("1. Add Task ")
     print("2. View Task ")
-    print("3. Update Tsk ")
-    print("4. Remove Task ")
+    print("3. Remove Task ")
+    print("4. Mark the Task as Done")
     print("5. Exit")
 
 while True:
+    
     display_menu()
     choice=int(input("Choose what task you want to perform (1-5): "))
     if choice==1:
@@ -17,9 +41,9 @@ while True:
     elif choice==2:
         view_task()
     elif choice==3:
-        update_task()
-    elif choice==4:
         remove_task()
+    elif choice==4:
+        mark_done_task()
     elif choice==5:
         print("Program Exited!")
         break
